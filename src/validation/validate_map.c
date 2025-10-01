88638080701c4790ad735f6dd9ae605ef2b1c3c8
Rami Mhazres <rmhazres@f1r6s9.codam.nl>
2025-10-01 16:20:32 +0200

    pushing from codam

--- src/validation/validate_map.c
+++ src/validation/validate_map.c
@@ -6,11 +6,41 @@
 /*   By: rmhazres <rmhazres@student.codam.nl>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/30 14:27:58 by rmhazres          #+#    #+#             */
-/*   Updated: 2025/10/01 13:17:43 by rmhazres         ###   ########.fr       */
+/*   Updated: 2025/10/01 14:53:56 by rmhazres         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
+
+int	is_matching(char c)
+{
+	
+}
+int	map_char_check(char **map)
+{
+	int	i;
+	int j;
+
+	i = 0;
+	while(map[i])
+	{
+		j = 0;
+		while(map[i][j])
+		{
+			if (map[i][j] != '0' && map[i][j] != '1'
+				&& map[i][j] != 'N' && map[i][j] != 'P'
+				&& map[i][j] != 'W' && map[i][j] != 'E'
+				&& map[i][j] != 'S' && map[i][j] != ' ')
+			{
+				ft_printf("Error\n Unknown charecter %c \n", map[i][j]);
+				return (FAILURE);	
+			}
+			j++;
+		}
+		i++;
+	}
+	return(SUCCESS);
+}
 
 int validate_map(t_config *config)
 {
@@ -19,5 +49,8 @@ int validate_map(t_config *config)
 		ft_printf("Error\nmap empty or doesnt exist!\n");
 		return (FAILURE);
 	}
+	if (map_char_check(config->map.grid) == FAILURE)
+		return(FAILURE);
+	
 	return (SUCCESS);
 }
