cf6fae52ee51c3eb300cedba5707e402969468df
Joey Baetsen <jbaetsen@f1r6s19.codam.nl>
2025-11-13 15:35:11 +0100

    refactored extract_map with a helper function to reduce total lines

--- src/utils/parse_config_utils.c
+++ src/utils/parse_config_utils.c
@@ -6,11 +6,17 @@
 /*   By: jbaetsen <jbaetsen@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/11/13 12:55:05 by jbaetsen      #+#    #+#                 */
-/*   Updated: 2025/11/13 13:05:37 by jbaetsen      ########   odam.nl         */
+/*   Updated: 2025/11/13 15:32:13 by jbaetsen      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
+
+void	free_and_get_line(t_config *config, char **line, int *i)
+{
+	free(*line);
+	*line = getnl_string(config->setting, i);
+}
 
 int	get_count(t_config *config, int i)
 {
