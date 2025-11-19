c2de25c992cf72fadbb85ab03f6df0ca19e85b5b
Joey Baetsen <jbaetsen@f1r1s17.codam.nl>
2025-11-19 13:06:08 +0100

    removed extra newline for norminette.

--- includes/utils.h
+++ includes/utils.h
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   utils.h                                            :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: rmhazres <rmhazres@student.codam.nl>       +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/09/29 12:22:42 by rmhazres          #+#    #+#             */
-/*   Updated: 2025/11/18 10:51:44 by rmhazres         ###   ########.fr       */
+/*                                                        ::::::::            */
+/*   utils.h                                            :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: rmhazres <rmhazres@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2025/09/29 12:22:42 by rmhazres      #+#    #+#                 */
+/*   Updated: 2025/11/19 13:05:47 by jbaetsen      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -66,7 +66,6 @@ int		init_game(t_game *game, t_config *config);
 void	clean_game(t_game *game);
 void	clean_cub3d(void *param, char *msg);
 int		atoi_p(const char *str);
-
 
 // flood_fill_utils.c
 bool	is_inside_map(t_config *config, int y, int x);
