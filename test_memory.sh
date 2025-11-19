468e44de59a711111a5c0355a354a1616220e14a
Joey Baetsen <jbaetsen@f1r1s17.codam.nl>
2025-11-19 13:19:08 +0100

    added memory valgrind for invalid maps

---
+++ test_memory.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+maps=$(find ./map/invalid -type f -name '*.cub')
+validmaps=$(find ./map/valid -type f -name '*.cub')
+
+for map in $maps; do
+echo "Testing: <$map>"
+valgrind ./cub3D $map
+echo
+done
